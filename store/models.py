9a33a9fe4701f1659a15b26160031e79145d41af
davidMaster <58896844+davidMaster@users.noreply.github.com>
2020-01-06 20:16:09 +0545

    created models.py

--- store/models.py
+++ store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class product(models.Model):
+    Product_Name = models.CharField(max_length=120)
+    Product_Type = models.CharField(max_length=120)
+    Product_Price = models.DecimalField(max_digits=20000,decimal_places=3)
+    Product_Description = models.TextField(blank=True)
+    Product_Quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.Product_Name
