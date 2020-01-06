d771ee87446b724afeac3477a37aa2300ffd8149
Nidhi <Nidhi@users.noreply.github.com>
2020-01-06 18:49:53 +0545

    Update urls and views

--- store/views.py
+++ store/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def store(request):
     return render(request,'store.html')
+
+def admin_page(request):
+    return render(request,'Admin_CRUD_Products.html')
     
