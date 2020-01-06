0f3f141b3b1242c239dbeb7360f7772fcb88e751
davidMaster <58896844+davidMaster@users.noreply.github.com>
2020-01-06 20:54:31 +0545

    Created database and shown in table

--- store/views.py
+++ store/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from store.models import product
 
 # Create your views here.
 def store(request):
     return render(request,'store.html')
 
 def admin_page(request):
-    return render(request,'Admin_CRUD_Products.html')
+    obj=product.objects.all()  
+    return render(request,'Admin_CRUD_Products.html',{'product':obj})
     
