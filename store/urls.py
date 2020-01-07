e6b38bbe0890c41ba2ce31a3035ea4447ed4b425
Nidhi <Nidhi@users.noreply.github.com>
2020-01-07 21:31:26 +0545

    update update_products

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,9 @@ from .views import *
 urlpatterns = [
     path('', store , name='store'),
     path('admin_products/',admin_page, name='admin'),
-    path('up/',upload),
-    path('up/upload',uploaded_save),
+    path('admin_products/upload/',upload),
+    path('admin_products/upload/save/',uploaded_save),
+    path('admin_products/edit/<int:id>',get_product),
+    path('admin_products/edit/update/<int:id>', update_product)
 ]
+
--- store/views.py
+++ store/views.py
@@ -8,7 +8,7 @@ def store(request):
 
 def admin_page(request):
     obj=product.objects.all()  
-    return render(request,'Admin_CRUD_Products.html',{'product':obj})
+    return render(request,'CRUD_Inventory/Admin_CRUD_Products.html',{'product':obj})
 
 def upload(request):
     return render(request,'CRUD_Inventory/upload.html')
@@ -21,5 +21,18 @@ def uploaded_save(request):
     qty=request.POST['Product_Quantity']
 
     product.objects.create(Product_Name=name, Product_Type=typ,Product_Price=price,Product_Quantity=qty)
-    obj=product.objects.all()
-    return render(request,'Admin_CRUD_Products.html',{'product':obj})
+    return HttpResponse("Saved")
+
+def get_product(request,id):
+    get_id = product.objects.get(id=id)
+    return render(request,'CRUD_Inventory/update.html',{'product':get_id})
+
+def update_product(request,id):
+    get_id = product.objects.get(id=id)
+    get_request = request.POST
+    get_id.Product_Name = request.POST['name']
+    get_id.Product_Type = request.POST['type']
+    get_id.Product_Price = request.POST['price']
+    get_id.Product_Quantity = request.POST['qty']
+    get_id.save()
+    return HttpResponse('Updated')
