974e836c415a0657668b66422a7385224a6a6333
Nidhi <Nidhi@users.noreply.github.com>
2020-01-10 22:21:31 +0545

    del abishek

--- store/urls.py
+++ store/urls.py
@@ -4,14 +4,11 @@ from .views import *
 
 
 urlpatterns = [
-    path('', store , name='store'),
-    path('admin_products/',admin_page, name='admin'),
-    path('admin_products/upload/',upload),
-    path('admin_products/upload/save/',uploaded_save),
+   
     path('admin_products/edit/<int:id>',get_product),
     path('admin_products/edit/update/<int:id>', update_product),
     path('admin_products/delete/<int:id>', delete_product),
-    path("admin_products/search/", search),
+   
     path('admin_products/uploaded_file/',upload_list),
     path('admin_products/upload_file/',upload_file),
     path("admin_products/uploaded_file/delet/<int:id>",run)
--- store/views.py
+++ store/views.py
@@ -6,25 +6,7 @@ from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
-def store(request):
-    return render(request,'store.html')
 
-def admin_page(request):
-    obj=product.objects.all()  
-    return render(request,'CRUD_Inventory/Admin_CRUD_Products.html',{'product':obj})
-
-def upload(request):
-    return render(request,'CRUD_Inventory/upload.html')
-    
-def uploaded_save(request):
-    getall=request.POST
-    name=request.POST['Product_Name']
-    typ=request.POST['Product_Type']
-    price=request.POST['Product_Price']
-    qty=request.POST['Product_Quantity']
-
-    product.objects.create(Product_Name=name, Product_Type=typ,Product_Price=price,Product_Quantity=qty)
-    return HttpResponseRedirect('/store/admin_products/')
 
 def get_product(request,id):
     get_id = product.objects.get(id=id)
@@ -46,26 +28,7 @@ def delete_product(request,id):
     x = product.objects.get(id = id)
     x.delete()
     return HttpResponseRedirect('/store/admin_products/')
-   
-def search(request):
-    if request.method == 'GET':
-        search = request.GET['searchs']
 
-        if search:
-            match = product.objects.filter(
-                Q( Product_Name__icontains=search) | Q(Product_Type__icontains=search)
-            )
-
-            if match:
-                return render(request,'CRUD_Inventory/Admin_CRUD_Products.html',{'sr':match})
-
-            else:
-                messages.error(request,'No Result Found')
-
-        else:
-            return HttpResponseRedirect('/store/admin_products/')
-
-    return render(request, 'CRUD_Inventory/Admin_CRUD_Products.html')
 
 def upload_list(request):
     list_upload = upload_files.objects.all()
