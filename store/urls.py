c843eb3b421080a442e66d7195d09ef3d16c25fa
Nidhi <Nidhi@users.noreply.github.com>
2020-01-24 13:38:10 +0545

    Update

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('', store , name='store'),
+    path("admin_products/restricted/",admin_restricted),
     path('admin_products/',admin_page, name='admin'),
     path('admin_products/upload/',upload),
     path('admin_products/upload/save/',uploaded_save),
@@ -12,9 +13,13 @@ urlpatterns = [
     path('admin_products/edit/update/<int:id>', update_product),
     path('admin_products/delete/<int:id>', delete_product),
     path("admin_products/search/", search),
+    path("search/",store_search),
     path('admin_products/uploaded_file/',upload_list),
     path('admin_products/upload_file/',upload_file),
-    path("admin_products/uploaded_file/delet/<int:id>",run)
+    path("admin_products/uploaded_file/delete/<int:id>",run),
+    path("page/<int:pageNo>/",pagination),
+    path("admin_products/page/<int:pageNo>/",paginationForCRUD)
+    
 ]
 
 
--- store/views.py
+++ store/views.py
@@ -1,29 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect,reverse
 from store.models import *
 from django.http import *
 from django.db.models import Q
 from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
+from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
 def store(request):
-    return render(request,'store.html')
+    get_spec = product.objects.all()[0:8]
+    return render(request,'store.html',{'prod':get_spec})
+# ,{'v_img':get_v}
+# def store_add(request,id):
+#     add_id = product.objects.get(id=id)
+#     return render(request,'cart.html',{'s_id':add_id})
 
+
+
+@permission_required('store.add_product', login_url='restricted/')
 def admin_page(request):
-    obj=product.objects.all()  
+    obj=product.objects.all()[0:8]  
     return render(request,'CRUD_Inventory/Admin_CRUD_Products.html',{'product':obj})
+
+def admin_restricted(request):
+    return render(request,'CRUD_Inventory/restricted.html')
 
 def upload(request):
     return render(request,'CRUD_Inventory/upload.html')
     
 def uploaded_save(request):
-    getall=request.POST
-    name=request.POST['Product_Name']
-    typ=request.POST['Product_Type']
-    price=request.POST['Product_Price']
-    qty=request.POST['Product_Quantity']
+    if request.method == 'POST':
+        getall=request.POST
+        name=request.POST['Product_Name']
+        typ=request.POST['Product_Type']
+        price=request.POST['Product_Price']
+        qty=request.POST['Product_Quantity']
+        imagee = request.FILES['Product_Image']
 
-    product.objects.create(Product_Name=name, Product_Type=typ,Product_Price=price,Product_Quantity=qty)
+        product.objects.create(Product_Name=name, Product_Type=typ,Product_Price=price,Product_Quantity=qty, img=imagee )
     return HttpResponseRedirect('/store/admin_products/')
 
 def get_product(request,id):
@@ -67,6 +81,26 @@ def search(request):
 
     return render(request, 'CRUD_Inventory/Admin_CRUD_Products.html')
 
+def store_search(request):
+    if request.method == 'GET':
+        search = request.GET['search']
+
+        if search:
+            match = product.objects.filter(
+                Q( Product_Name__icontains=search) | Q(Product_Type__icontains=search)
+            )
+
+            if match:
+                return render(request,'store.html',{'search':match})
+
+            else:
+                messages.error(request,'No Result Found')
+
+        else:
+            return HttpResponseRedirect('/store/')
+
+    return render(request, 'store.html')
+
 def upload_list(request):
     list_upload = upload_files.objects.all()
     return render(request,'CRUD_Inventory/uploaded.html',{'lists':list_upload})
@@ -91,6 +125,68 @@ def run(request,id):
 
 # def uploaded_file(request):
 #     return render(request,)
-   
+
+
+def pagination(request,pageNo):
+    get_all = product.objects.all()
+    if request.method == "GET":
+        if pageNo == 1:
+            pageNo=0
+            count=0
+            for i in range(8):
+                try:
+                    if get_all.get(id=i+1) is not None:
+                       count=count+1
+                    else:
+                       break
+                except:
+                    pass
+            get_spec=get_all[pageNo:count]
+            return render(request,'store.html',{'v_img':get_spec})
+        else:
+            count=0
+            i=(pageNo-1)*8
+            for i in range(pageNo*8):
+                try:
+                    if get_all.get(id=i+1) is not None:
+                        count=i+1
+                    else:
+                        break
+                except:
+                    pass
+            get_spec=get_all[(pageNo-1)*8:count]
+            return render(request,'store.html',{'v_img':get_spec})
+
+
+def paginationForCRUD(request,pageNo):
+    get_all=product.objects.all()
+    if request.method == "GET":
+        if pageNo == 1:
+            pageNo=0
+            count=0
+            for i in range(8):
+                try:
+                    if get_all.get(id=i+1) is not None:
+                       count=count+1
+                    else:
+                       break
+                except:
+                    pass
+            get_spec=get_all[pageNo:count]
+            return render(request,'CRUD_Inventory/Admin_CRUD_Products.html',{'prod':get_spec})
+        else:
+            count=0
+            i=(pageNo-1)*8
+            for i in range(pageNo*8):
+                try:
+                    if get_all.get(id=i+1) is not None:
+                        count=i+1
+                    else:
+                        break
+                except:
+                    pass
+            get_spec=get_all[(pageNo-1)*8:count]
+            return render(request,'CRUD_Inventory/Admin_CRUD_Products.html',{'prod':get_spec})
+
 
 
